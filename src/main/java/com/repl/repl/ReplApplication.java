9a26593e5bbf89992664b1ac6bdf66f9f4f552be
BuildTools <unconfigured@null.spigotmc.org>
2023-06-20 15:59:36 +0900

    Initial commit

---
+++ src/main/java/com/repl/repl/ReplApplication.java
@@ -0,0 +1,13 @@
+package com.repl.repl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplApplication.class, args);
+	}
+
+}
