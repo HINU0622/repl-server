781c11e24f9e02b6da2c8e0cca063b35707ccc20
HINU0622 <HINU0622@gmail.com>
2023-06-21 16:02:40 +0900

    왜 기여 안 느냐

--- src/main/java/com/repl/repl/repository/ServeyRepository.java
+++ src/main/java/com/repl/repl/repository/ServeyRepository.java
@@ -4,4 +4,5 @@ import com.repl.repl.entity.ServeyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ServeyRepository extends JpaRepository<ServeyEntity, String> {
+
 }
