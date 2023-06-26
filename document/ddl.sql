c6390433e00e7e2c0093c04c4916784177489c66
HINU0622 <HINU0622@gmail.com>
2023-06-26 12:34:34 +0900

    reply 테이블 생성

--- document/ddl.sql
+++ document/ddl.sql
@@ -7,9 +7,20 @@ CREATE TABLE user(
 DROP TABLE user;
 
 CREATE TABLE servey(
-	servey_id INT(36) NOT NULL,
+	servey_id VARCHAR(36) NOT NULL,
     user_id VARCHAR(32) NOT NULL,
+    content VARCHAR(100) NOT NULL,
     PRIMARY KEY(servey_id)
 );
 
 DROP TABLE servey;
+
+CREATE TABLE reply(
+	reply_id VARCHAR(36) NOT NULL,
+    servey_id VARCHAR(36) NOT NULL,
+    user_id VARCHAR(32) NOT NULL,
+    content VARCHAR(100) NOT NULL,
+    PRIMARY KEY(reply_id)
+);
+
+DROP TABLE reply;
